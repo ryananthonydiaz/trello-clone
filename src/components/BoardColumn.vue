08ec5686a7c2dcac660fb8deb91e81d673ea6cac
Ryan Diaz <email@ryananthonydiaz.com>
2020-09-06 19:38:52 -0700

    Mixin created for shared methods and props across column and task.

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -20,7 +20,6 @@
         :columnIndex="columnIndex"
         :board="board"
       />
-
       <input
         type="text"
         class="block p-2 w-full bg-transparent"
@@ -33,55 +32,13 @@
 
 <script>
 import ColumnTask from './ColumnTask'
-import { CREATE_TASK, MOVE_TASK, MOVE_COLUMN } from '../store/types'
+import { CREATE_TASK } from '../store/types'
+import tasksAndColumns from '../mixins/tasksAndColumns'
 
 export default {
-  components: {
-    ColumnTask
-  },
-  props: {
-    column: {
-      type: Object,
-      required: true
-    },
-    columnIndex: {
-      type: Number,
-      required: true
-    },
-    board: {
-      type: Object,
-      required: true
-    }
-  },
+  components: { ColumnTask },
+  mixins: [tasksAndColumns],
   methods: {
-    moveTaskOrColumn (e, toTasks, toColumnIndex, toTaskIndex) {
-      const type = e.dataTransfer.getData('type')
-      if (type === 'task') {
-        this.moveTask(e, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTasks.length)
-      } else {
-        this.moveColumn(e, toColumnIndex)
-      }
-    },
-    moveTask (e, toTasks, toTaskIndex) {
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      const fromTasks = this.board.columns[fromColumnIndex].tasks
-      const fromTaskIndex = e.dataTransfer.getData('from-task-index')
-
-      this.$store.commit(MOVE_TASK, {
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex
-      })
-    },
-    moveColumn (e, toColumnIndex) {
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-
-      this.$store.commit(MOVE_COLUMN, {
-        toColumnIndex,
-        fromColumnIndex
-      })
-    },
     pickupColumn (e, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'
       e.dataTransfer.dropEffect = 'move'
